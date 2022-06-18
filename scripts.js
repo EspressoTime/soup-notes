84302ce43fc6dd2e3010ad990db11be18502b993
EspressoTime <EspressoTime@users.noreply.github.com>
2022-06-18 02:23:38 -0700

    JPG > SVG

--- scripts.js
+++ scripts.js
@@ -1,76 +1,77 @@
 const map = {
-  A: "assets/A.jpeg",
-  B: "assets/B.jpeg",
-  C: "assets/C.jpeg",
-  D: "assets/D.jpeg",
-  E: "assets/E.jpeg",
-  F: "assets/F.jpeg",
-  G: "assets/G.jpeg",
-  H: "assets/H.jpeg",
-  I: "assets/I.jpeg",
-  J: "assets/J.jpeg",
-  K: "assets/K.jpeg",
-  L: "assets/L.jpeg",
-  M: "assets/M.jpeg",
-  N: "assets/N.jpeg",
-  O: "assets/O.jpeg",
-  P: "assets/P.jpeg",
-  Q: "assets/Q.jpeg",
-  R: "assets/R.jpeg",
-  S: "assets/S.jpeg",
-  T: "assets/T.jpeg",
-  U: "assets/U.jpeg",
-  V: "assets/V.jpeg",
-  W: "assets/W.jpeg",
-  X: "assets/X.jpeg",
-  Y: "assets/Y.jpeg",
-  Z: "assets/Z.jpeg",
-  0: "assets/0.jpeg",
-  1: "assets/1.jpeg",
-  2: "assets/2.jpeg",
-  3: "assets/3.jpeg",
-  4: "assets/4.jpeg",
-  5: "assets/5.jpeg",
-  6: "assets/6.jpeg",
-  7: "assets/7.jpeg",
-  8: "assets/8.jpeg",
-  9: "assets/9.jpeg",
-  ".": "assets/dot.jpeg",
-  "-": "assets/dash.jpeg",
-  "(": "assets/paren_L.jpeg",
-  ")": "assets/paren_R.jpeg",
-  "+": "assets/plus.jpeg",
-  "=": "assets/equal.jpeg",
-  "*": "assets/asterisk.jpeg",
-  "!": "assets/bang.jpeg",
-  "?": "assets/question.jpeg",
+  A: "assets/A.svg",
+  B: "assets/B.svg",
+  C: "assets/C.svg",
+  D: "assets/D.svg",
+  E: "assets/E.svg",
+  F: "assets/F.svg",
+  G: "assets/G.svg",
+  H: "assets/H.svg",
+  I: "assets/I.svg",
+  J: "assets/J.svg",
+  K: "assets/K.svg",
+  L: "assets/L.svg",
+  M: "assets/M.svg",
+  N: "assets/N.svg",
+  O: "assets/O.svg",
+  P: "assets/P.svg",
+  Q: "assets/Q.svg",
+  R: "assets/R.svg",
+  S: "assets/S.svg",
+  T: "assets/T.svg",
+  U: "assets/U.svg",
+  V: "assets/V.svg",
+  W: "assets/W.svg",
+  X: "assets/X.svg",
+  Y: "assets/Y.svg",
+  Z: "assets/Z.svg",
+  0: "assets/0.svg",
+  1: "assets/1.svg",
+  2: "assets/2.svg",
+  3: "assets/3.svg",
+  4: "assets/4.svg",
+  5: "assets/5.svg",
+  6: "assets/6.svg",
+  7: "assets/7.svg",
+  8: "assets/8.svg",
+  9: "assets/9.svg",
+  ".": "assets/dot.svg",
+  "-": "assets/dash.svg",
+  "(": "assets/paren_L.svg",
+  ")": "assets/paren_R.svg",
+  "+": "assets/plus.svg",
+  "=": "assets/equal.svg",
+  "*": "assets/asterisk.svg",
+  "!": "assets/bang.svg",
+  "?": "assets/question.svg",
 };
 
 const textarea = document.querySelector("#textarea");
 const target = document.querySelector("#target");
+textarea.addEventListener('input', updateValues);
 
-textarea.onkeyup = function () {
-  this.value = this.value.replace(/[^a-zA-Z0-9 \-+\n=*!?)(\.]/, "");
+function updateValues(e) {
+    e.target.value = e.target.value.replace(/[^a-zA-Z0-9 \-+\n=*!?)(\.]/, "");
 
   target.innerText = "";
-  if (this.value.length > 20) {
+  if (e.target.value.length > 20) {
     document.querySelector('#target').classList.add("smaller");
   }
   else {
     document.querySelector('#target').classList.remove("smaller");
   }
-  [...this.value].forEach((e, i) => {
+  [...e.target.value].forEach((ea, i) => {
     //manage spaces
-    if (e === " ") {
+    if (ea === " ") {
       target.insertAdjacentHTML("beforeend", `<div class="spacer"></div>`);
     }
     //manage newline
-    if (e === "\n"){
+    if (ea === "\n"){
       target.insertAdjacentHTML("beforeend", `<br>`);
     }
     //manage characters
-    if (map[e.toUpperCase()]) {
-      const path = `<img src="${map[e.toUpperCase()]}" alt="${e}">`;
+    if (map[ea.toUpperCase()]) {
+      const path = `<img src="${map[ea.toUpperCase()]}" alt="${ea}">`;
       target.insertAdjacentHTML("beforeend", path);
     }
   });
